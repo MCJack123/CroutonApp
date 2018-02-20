a9746ae4c43784c1dd5684f4c27b0faa9c0d8b43
JackMacWindows <jackbruienne@yahoo.com>
2018-02-20 13:58:15 -0500

    Update addprogram.cpp

--- addprogram.cpp
+++ addprogram.cpp
@@ -4,6 +4,11 @@
 #include <string>
 #include <jsoncpp/json/json.h>
 
+static const std::string base64_chars = 
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
+
 std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
   std::string ret;
   int i = 0;
